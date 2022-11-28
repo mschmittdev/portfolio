b9186fa89db804dee30f870ce09f7ec8deb6ae51
Marcel Schmitt <schmitt.marcel97@gmail.com>
2022-11-28 18:04:58 +0100

    add pirsch analytics

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <script defer type="text/javascript" src="https://api.pirsch.io/pirsch.js"
+                    id="pirschjs"
+                    data-code="4p8jVcHjstSl5ieePDap5AQln3DqfIMp"></script>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
